c5ec380dd5626636e2273eed1ba4ddbb82ed2fbd
Oleg Kireev <okireev@ubic.tech>
2022-07-21 14:10:03 +0300

    @/components/GenderSelector: add markup

--- src/components/GenderSelector/index.tsx
+++ src/components/GenderSelector/index.tsx
@@ -1,7 +1,40 @@
+import { Title } from "../Title";
+import styles from './styles.module.scss';
+
 export function GenderSelector() {
   return (
-    <div>
-      GenderSelector
+    <div className={styles.wrapper}>
+      <Title size="xs">Gender</Title>
+      <ul className={styles.list}>
+        <li>
+          <input
+            className={styles.radio}
+            type="radio"
+            id="gender-selecotor-male"
+            name="gender-selecotor"
+          />
+          <label
+            className={styles.icon}
+            htmlFor="gender-selecotor-male"
+          >
+            M
+          </label>
+        </li>
+        <li>
+          <input
+            className={styles.radio}
+            type="radio"
+            id="gender-selecotor-female"
+            name="gender-selecotor"
+          />
+          <label
+            className={styles.icon}
+            htmlFor="gender-selecotor-female"
+          >
+            F
+          </label>
+        </li>
+      </ul>
     </div>
   )
 }
