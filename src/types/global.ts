93dd6985cd3da36cd40101ee9f0d35ffee476510
Oleg Kireev <okireev@ubic.tech>
2022-08-18 14:06:52 +0300

    @/types/global: fix RaceType typo

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,6 +1,6 @@
 export type SideType = 'Alliance' | 'Horde';
 
-export type RaceType = 'Hunan' | 'Dwarf' | 'Night Elf' | 'Gnome' | 'Draenei' | 'Orc' | 'Undead' | 'Tauren' | 'Troll' | 'Blood Elf';
+export type RaceType = 'Human' | 'Dwarf' | 'Night Elf' | 'Gnome' | 'Draenei' | 'Orc' | 'Undead' | 'Tauren' | 'Troll' | 'Blood Elf';
 
 export type ClassType = 'Warrior' | 'Rogue' | 'Mage' | 'Warlock' | 'Shaman' | 'Hunter' | 'Paladin' | 'Priest' | 'Druid';
 
