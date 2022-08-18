8a2d1bba3d9f1ebc4caf9003aa7cde1fcbfbe2c7
Oleg Kireev <okireev@ubic.tech>
2022-08-18 14:33:15 +0300

    @/features/character-creation/components/RaceSelector: display all races

--- src/features/character-creation/components/RaceSelector/index.tsx
+++ src/features/character-creation/components/RaceSelector/index.tsx
@@ -14,45 +14,56 @@ export function RaceSelector() {
     });
   };
 
+  const allianceRaces: RaceType[] = ['human', 'dwarf', 'gnome', 'night-elf', 'draenei'];
+  const hordeRaces: RaceType[] = ['orc', 'troll', 'undead', 'tauren', 'blood-elf'];
+
   return (
     <div className={styles.wrapper}>
       <Title size="xs">Race</Title>
-      <ul className={styles.list}>
-        <li>
-          <input
-            className={styles.radio}
-            type="radio"
-            id="race-selector-human"
-            name="race-selector"
-            value="human"
-            checked={value === 'human'}
-            onChange={handleChange}
-          />
-          <label
-            className={styles.icon}
-            htmlFor="race-selector-human"
-          >
-            Human
-          </label>
-        </li>
-        <li>
-          <input
-            className={styles.radio}
-            type="radio"
-            id="race-selector-orc"
-            name="race-selector"
-            value="orc"
-            checked={value === 'orc'}
-            onChange={handleChange}
-          />
-          <label
-            className={styles.icon}
-            htmlFor="race-selector-orc"
-          >
-            Orc
-          </label>
-        </li>
-      </ul>
+      <div className={styles.raceLists}>
+        <ul className={styles.list}>
+          {allianceRaces.map((race) => (
+            <li>
+              <input
+                className={styles.radio}
+                type="radio"
+                id={`race-selector-${race}`}
+                name="race-selector"
+                value={race}
+                checked={value === race}
+                onChange={handleChange}
+              />
+              <label
+                className={styles.icon}
+                htmlFor={`race-selector-${race}`}
+              >
+                {race}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <ul className={styles.list}>
+          {hordeRaces.map((race) => (
+            <li>
+              <input
+                className={styles.radio}
+                type="radio"
+                id={`race-selector-${race}`}
+                name="race-selector"
+                value={race}
+                checked={value === race}
+                onChange={handleChange}
+              />
+              <label
+                className={styles.icon}
+                htmlFor={`race-selector-${race}`}
+              >
+                {race}
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
