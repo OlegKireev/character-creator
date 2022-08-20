430d28632d2474b0ec82bd4e2e0e1ece6f917877
Oleg Kireev <okireev@ubic.tech>
2022-08-20 10:53:20 +0300

    @/App: import global theme scss file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CharacterCreation } from 'features/character-creation';
 import { Provider } from 'react-redux';
 import { store } from 'store';
 import { Layout } from './components/Layout';
+import './theme/index.scss';
 
 function App() {
   return (
