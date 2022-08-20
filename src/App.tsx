f2baf9e155cebfc3c127c32ba6cd346ed3501d60
Oleg Kireev <okireev@ubic.tech>
2022-08-20 09:45:15 +0300

    @/App: wrap with the redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { CharacterCreation } from 'features/character-creation';
+import { Provider } from 'react-redux';
+import { store } from 'store';
 import { Layout } from './components/Layout';
 
 function App() {
   return (
-    <Layout>
-      <CharacterCreation />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <CharacterCreation />
+      </Layout>
+    </Provider>
   );
 }
 
