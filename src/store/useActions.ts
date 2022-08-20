f66cc7f4d129cb9ac772cd0ad2ba33a8218e8fff
Oleg Kireev <okireev@ubic.tech>
2022-08-20 09:47:08 +0300

    @/store/{useActions, useTypedSelector}: create

---
+++ src/store/useActions.ts
@@ -0,0 +1,13 @@
+import { bindActionCreators } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import { characterCreationActions } from 'features/character-creation/store';
+
+const allActions = {
+  ...characterCreationActions,
+};
+
+export const useActions = () => {
+  const dispatch = useDispatch();
+
+  return bindActionCreators(allActions, dispatch);
+};
