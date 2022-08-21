d1756673c1715f32ebab3a20f76c7a23bf8485b8
Oleg Kireev <okireev@ubic.tech>
2022-08-21 16:00:08 +0300

    @/constants/races: add availableClasses field

--- src/constants/races.ts
+++ src/constants/races.ts
@@ -1,4 +1,5 @@
 import {
+  ClassType,
   RaceType, SideType,
 } from 'types/global';
 
@@ -6,46 +7,57 @@ export const RACES: {
   id: number,
   name: RaceType,
   side: SideType,
+  availableClasses: ClassType[]
 }[] = [{
   id: 1,
   name: 'human',
   side: 'alliance',
+  availableClasses: ['warrior', 'paladin', 'rogue', 'priest', 'mage', 'warlock'],
 }, {
   id: 2,
   name: 'orc',
   side: 'horde',
+  availableClasses: ['warrior', 'hunter', 'rogue', 'shaman', 'warlock'],
 }, {
   id: 3,
   name: 'dwarf',
   side: 'alliance',
+  availableClasses: ['warrior', 'paladin', 'hunter', 'rogue', 'priest'],
 }, {
   id: 4,
   name: 'night-elf',
   side: 'alliance',
+  availableClasses: ['warrior', 'hunter', 'rogue', 'priest', 'druid'],
 }, {
   id: 5,
   name: 'undead',
   side: 'horde',
+  availableClasses: ['warrior', 'rogue', 'priest', 'mage', 'warlock'],
 }, {
   id: 6,
   name: 'tauren',
   side: 'horde',
+  availableClasses: ['warrior', 'hunter', 'shaman', 'druid'],
 }, {
   id: 7,
   name: 'gnome',
   side: 'alliance',
+  availableClasses: ['warrior', 'rogue', 'mage', 'warlock'],
 }, {
   id: 8,
   name: 'troll',
   side: 'horde',
+  availableClasses: ['warrior', 'hunter', 'rogue', 'priest', 'shaman', 'mage'],
 }, {
   id: 10,
   name: 'blood-elf',
   side: 'horde',
+  availableClasses: ['paladin', 'hunter', 'rogue', 'priest', 'mage', 'warlock'],
 }, {
   id: 11,
   name: 'draenei',
   side: 'alliance',
+  availableClasses: ['warrior', 'paladin', 'hunter', 'priest', 'shaman', 'mage'],
 }];
 
 export const ALLIANCE_RACES = RACES.filter((race) => race.side === 'alliance');
