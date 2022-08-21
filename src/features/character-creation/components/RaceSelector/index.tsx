dd9ae5ee4df42cd20b6fa2dc7adb64eaab187829
Oleg Kireev <okireev@ubic.tech>
2022-08-21 16:09:15 +0300

    @/features/character-creation/components/RaceSelector: use useRaces

--- src/features/character-creation/components/RaceSelector/index.tsx
+++ src/features/character-creation/components/RaceSelector/index.tsx
@@ -1,8 +1,8 @@
 import cx from 'classnames';
 import { Radio } from 'components/Radio';
 import { Title } from 'components/Title';
-import { ALLIANCE_RACE_NAMES, HORDE_RACE_NAMES } from 'constants/races';
 import { selectCharacterCreation } from 'features/character-creation/store/selectors';
+import { useRaces } from 'hooks/useRaces';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useActions } from 'store/useActions';
@@ -10,6 +10,7 @@ import { RaceType } from 'types/global';
 import styles from './styles.module.scss';
 
 export function RaceSelector() {
+  const { allianceRaceNames, hordeRaceNames } = useRaces();
   const value = useSelector(selectCharacterCreation).race;
   const { updateCharacterCreation } = useActions();
 
@@ -24,7 +25,7 @@ export function RaceSelector() {
       <Title size="md" variant="secondary">Race</Title>
       <div className={styles.raceLists}>
         <ul className={cx(styles.list, styles.alliance)}>
-          {ALLIANCE_RACE_NAMES.map((race) => (
+          {allianceRaceNames.map((race) => (
             <li key={`race-${race}`}>
               <Radio
                 name="race-selector"
@@ -38,7 +39,7 @@ export function RaceSelector() {
           ))}
         </ul>
         <ul className={cx(styles.list, styles.horde)}>
-          {HORDE_RACE_NAMES.map((race) => (
+          {hordeRaceNames.map((race) => (
             <li key={`race-${race}`}>
               <Radio
                 name="race-selector"
