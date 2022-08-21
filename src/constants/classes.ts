c35b4ec5ff81f3e6a203af39c1a4448e65086bbc
Oleg Kireev <okireev@ubic.tech>
2022-08-21 16:26:47 +0300

    @/constants/classes: create

---
+++ src/constants/classes.ts
@@ -0,0 +1,43 @@
+import { ClassType } from 'types/global';
+
+export const CLASSES: {
+  id: number;
+  name: ClassType,
+  label: string,
+}[] = [{
+  id: 1,
+  name: 'warrior',
+  label: 'Warrior',
+}, {
+  id: 2,
+  name: 'paladin',
+  label: 'Paladin',
+}, {
+  id: 3,
+  name: 'hunter',
+  label: 'Hunter',
+}, {
+  id: 4,
+  name: 'rogue',
+  label: 'Rogue',
+}, {
+  id: 5,
+  name: 'shaman',
+  label: 'Shaman',
+}, {
+  id: 6,
+  name: 'druid',
+  label: 'Druid',
+}, {
+  id: 7,
+  name: 'priest',
+  label: 'Priest',
+}, {
+  id: 8,
+  name: 'mage',
+  label: 'Mage',
+}, {
+  id: 9,
+  name: 'warlock',
+  label: 'Warlock',
+}];
