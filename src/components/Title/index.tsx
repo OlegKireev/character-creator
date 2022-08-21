5c0d68cf41a83ad7ad91b1c2b90cfb690a9de0c4
Oleg Kireev <okireev@ubic.tech>
2022-08-21 09:17:58 +0300

    @/components/Title: add 'variant', 'as' props

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,14 +4,29 @@ import styles from './styles.module.scss';
 interface IProps {
   children: React.ReactNode
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  variant?: 'primary' | 'secondary';
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
 }
 
-export function Title({ children, size = 'md' }: IProps) {
+export function Title({
+  children,
+  size = 'xl',
+  variant = 'primary',
+  as = 'h2',
+}: IProps) {
+  const TitleTag = as;
   return (
     <div className={styles.wrapper}>
-      <h1 className={cx(styles.title, styles[size])} data-text={children}>
+      <TitleTag
+        className={cx(
+          styles.title,
+          styles[size],
+          styles[variant],
+        )}
+        data-text={children}
+      >
         {children}
-      </h1>
+      </TitleTag>
     </div>
   );
 }
